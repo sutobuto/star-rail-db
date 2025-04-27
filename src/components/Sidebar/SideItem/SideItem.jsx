6a66a46fa71b242ce949f75ac0f039726ea4cba5
Lian Estabillo <estabillog8@gmail.com>
2025-04-27 20:08:42 +0800

    Page routing setup. Reworked media queries and RWD for character page.

--- src/components/Sidebar/SideItem/SideItem.jsx
+++ src/components/Sidebar/SideItem/SideItem.jsx
@@ -3,7 +3,12 @@ import styles from "./SideItem.module.css";
 const SideItem = (props) => {
     return (
         <button className={styles.container}>
-            <span className={styles.icon}>icon</span>
+            <div className={styles.icon}>
+                <img 
+                    width='24'
+                    src={`./src/assets/imgs/ui/icons/${props.icon}`} 
+                ></img>
+            </div>
             <span className={styles.name} style={{}}>{props.name}</span>
         </button>
     )
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,20 +1,32 @@
 import styles from './Sidebar.module.css';
 import SideItem from './SideItem/SideItem';
 import { useState } from 'react';
+import { useMediaQuery } from '/src/hooks/useMediaQuery';
 
 const Sidebar = (props) => {
     const setCollapse = props.setCollapse;
+    const breakpoint = useMediaQuery('(max-width: 920px)');
+
     return (
-        <div className={`${styles.container} ${props.isCollapsed && styles.container_small}`}>
+        <div 
+            className={`${styles.container} ${props.isCollapsed && styles.container_small}`}
+            style={{display: breakpoint && 'none',}}
+        >
             <div className={styles.head}>
-                <button onClick={() => setCollapse(!props.isCollapsed)} className={styles.collapse}>col</button>
+                <button onClick={() => setCollapse(!props.isCollapsed)} className={styles.collapse}>
+                    <img src=
+                    {
+                        !props.isCollapsed ? `/src/assets/imgs/ui/icons/nav_close.svg`: `/src/assets/imgs/ui/icons/nav_open.svg`
+                    }
+                    ></img>
+                </button>
             </div>
             <div className={styles.sideitems}>
-                <SideItem name='Home' isCollapsed={props.isCollapsed}/>
-                <SideItem name='Characters' isCollapsed={props.isCollapsed}/>
-                <SideItem name='Light Cones' isCollapsed={props.isCollapsed}/>
-                <SideItem name='Relics' isCollapsed={props.isCollapsed}/>
-                <SideItem name='Bosses' isCollapsed={props.isCollapsed}/>
+                <SideItem name='Home' icon='home.svg' isCollapsed={props.isCollapsed}/>
+                <SideItem name='Characters' icon='characters.svg' isCollapsed={props.isCollapsed}/>
+                <SideItem name='Light Cones' icon='lightcones.webp' isCollapsed={props.isCollapsed}/>
+                <SideItem name='Relics' icon='relics.webp' isCollapsed={props.isCollapsed}/>
+                <SideItem name='Bosses' icon='bosses.svg' isCollapsed={props.isCollapsed}/>
             </div>
         </div>
     )
