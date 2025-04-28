05896ee54ffd46223bfac2f5c39073afb30e197f
sutobuto <estabillog8@gmail.com>
2025-04-28 17:51:14 +0800

    Refactored main content resizing. Moved sidebar collapse state one parent up.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import Characters from '/src/pages/Characters/Characters';
+import Home from "./pages/Characters/Home";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { useState } from "react";
 
 function App() {
+  const [isCollapsed, setCollapse] = useState(true);
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Characters />}/>
-        <Route path='/characters' element={<Characters />}/>
+        <Route path='/' element={<Home isCollapsed={isCollapsed} setCollapse={setCollapse}/>}/>
+        <Route path='/characters' element={<Characters isCollapsed={isCollapsed} setCollapse={setCollapse}/>} />
+        {/* <Route path='/lightcones' element={<Lightcones isCollapsed={isCollapsed} setCollapse={setCollapse}/>} />
+        <Route path='/relics' element={<Relics isCollapsed={isCollapsed} setCollapse={setCollapse}/>} />
+        <Route path='/bosses' element={<Bosses isCollapsed={isCollapsed} setCollapse={setCollapse}/>} /> */}
       </Routes>
     </Router>
   )
--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -5,22 +5,20 @@ import { useMediaQuery } from '/src/hooks/useMediaQuery';
 import styles from './Characters.module.css';
 import data from '/src/jsons/chardata.json';
 
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-
-function Characters() {
-  const [isCollapsed, setCollapse] = useState(true);
-  const breakpoint = useMediaQuery(isCollapsed ? '(max-width: 820px)' : '(max-width: 770px)');
+function Characters(props) {
+  const setCollapse = props.setCollapse
+  const breakpoint = useMediaQuery(props.isCollapsed ? '(max-width: 820px)' : '(max-width: 770px)');
   
   return (
     <>
-    <Sidebar isCollapsed={isCollapsed} setCollapse={setCollapse}/>
+    <Sidebar isCollapsed={props.isCollapsed} setCollapse={props.setCollapse}/>
     <div class='main' style={{
       transition: '0.4s', 
-      marginLeft: (isCollapsed && !breakpoint) && '5rem'
+      width: (props.isCollapsed && !breakpoint) && 'calc(100% - 5rem)'
       }}>
       <h1>Characters</h1>
       <div className={styles.characters}>
-        {data.map((char) => <CharCard name={char.Name} codename={char.Codename} type={char.Element} path={char.Path} rarity={char.Rarity} />)}
+        {data.sort((a, b) => a.Name > b.Name ? 1 : -1).map((char) => <CharCard name={char.Name} codename={char.Codename} type={char.Element} path={char.Path} rarity={char.Rarity} />)}
       </div>
     </div>
     </>
